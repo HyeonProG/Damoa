9b2548bc80fd7a839ee11399eeb1be98d214588d
Greenery367 <greenery3699@gmail.com>
2024-10-15 15:04:03 +0900

    계좌 등록 컨트롤러 추가

--- src/main/java/com/damoa/controller/AccountController.java
+++ src/main/java/com/damoa/controller/AccountController.java
@@ -1,4 +1,13 @@
 package com.damoa.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class AccountController {
+
+    @GetMapping("/account-list")
+    public String registerAccountPage(){
+        return "register_account";
+    }
 }
