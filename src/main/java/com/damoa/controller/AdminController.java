2dd87b5748a0a9bfcbec3ac8524fd11c0f6a80c9
haejujung <ysin70@naver.com>
2024-10-22 17:52:52 +0900

    회원관리 페이징 처리 구현중

--- src/main/java/com/damoa/controller/AdminController.java
+++ src/main/java/com/damoa/controller/AdminController.java
@@ -3,11 +3,10 @@ package com.damoa.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.damoa.dto.admin.AdminSignInDTO;
 import com.damoa.handler.exception.DataDeliveryException;
@@ -18,9 +17,9 @@ import com.damoa.service.AdminService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
@@ -37,15 +36,15 @@ public class AdminController {
      */
     @GetMapping("/main")
     public String mainPage(HttpServletRequest request) {
-        HttpSession session = request.getSession(false);
-        Admin admin = session != null ? (Admin) session.getAttribute("admin") : null;
-        
-        // 로그인을 하지 않았을 경우 로그인 페이지로 리다이렉트
-        if (admin == null) {
-            return "redirect:/admin/sign-in";
-        }
+//        HttpSession session = request.getSession(false);
+//        Admin admin = session != null ? (Admin) session.getAttribute("admin") : null;
+//
+//        // 로그인을 하지 않았을 경우 로그인 페이지로 리다이렉트
+//        if (admin == null) {
+//            return "redirect:/admin/sign-in";
+//        }
 
-        return "/admin/main";
+        return "/admin/admin_main";
     }
 
     /**
@@ -65,30 +64,59 @@ public class AdminController {
      * @param password
      * @return
      */
-    @PostMapping("/sign-in")
-    public String adminSignInProc(AdminSignInDTO adminSignInDTO, HttpServletRequest request) {
-        try {
-            Admin admin = adminService.findAdmin(adminSignInDTO);
-            // 세션 생성
-            HttpSession session = request.getSession(true);
-            session.setAttribute("admin", admin);
-            return "redirect:/admin/main";
-        } catch (Exception e) {
-            if (adminSignInDTO.getUsername() == null || adminSignInDTO.getUsername().isEmpty()) {
-                throw new DataDeliveryException("아이디를 입력하세요.", HttpStatus.BAD_REQUEST);
-            }
-            if (adminSignInDTO.getPassword() == null || adminSignInDTO.getPassword().isEmpty()) {
-                throw new DataDeliveryException("비밀번호를 입력하세요.", HttpStatus.BAD_REQUEST);
-            }
-            e.printStackTrace();
-            return "/admin/sign_in";
-        }
+//    @PostMapping("/sign-in")
+//    public String adminSignInProc(AdminSignInDTO adminSignInDTO, HttpServletRequest request) {
+//        try {
+//            Admin admin = adminService.findAdmin(adminSignInDTO);
+//            // 세션 생성
+//            HttpSession session = request.getSession(true);
+//            session.setAttribute("admin", admin);
+//            return "redirect:/admin/main";
+//        } catch (Exception e) {
+//            if (adminSignInDTO.getUsername() == null || adminSignInDTO.getUsername().isEmpty()) {
+//                throw new DataDeliveryException("아이디를 입력하세요.", HttpStatus.BAD_REQUEST);
+//            }
+//            if (adminSignInDTO.getPassword() == null || adminSignInDTO.getPassword().isEmpty()) {
+//                throw new DataDeliveryException("비밀번호를 입력하세요.", HttpStatus.BAD_REQUEST);
+//            }
+//            e.printStackTrace();
+//            return "/admin/sign_in";
+//        }
+//    }
+
+    @GetMapping("/management")
+    public String ListPage(Model model){
+        List<User> userList = adminService.getAllUser();
+        model.addAttribute("userList", userList);
+        return "admin/admin_user_list";
     }
 
-    @GetMapping("/faq")
-    public String faq(){
-        return "admin/admin_faq";
+    @GetMapping("/{currentPageNum}")
+    public String UserListPage(@PathVariable(required = false) int currentPageNum, Model model){
+
+        List<User> allUser = adminService.getAllUser();
+        int totalUser = allUser.size();
+        int limit = 10;
+        int totalPages = totalUser/limit;
+        int offset;
+
+        if(currentPageNum == 0 || currentPageNum == 1){
+            currentPageNum = 1;
+            offset = 0;
+        } else {
+            offset = currentPageNum*limit+1;
+        }
+
+        List<User> userList = adminService.getUserList(limit,offset);
+
+        model.addAttribute("userList",userList);
+        model.addAttribute("totalUser",totalUser);
+        model.addAttribute("totalPages",totalPages);
+        model.addAttribute("currentPageNum",currentPageNum);
+
+        return "/admin/admin_notice_list";
     }
+
 
 
 }
