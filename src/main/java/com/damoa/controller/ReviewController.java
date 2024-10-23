4aed813a3b5cec3d0bec9ced650d2d8c92ae5029
Myeongse <jeonmyungse@naver.com>
2024-10-23 12:54:15 +0900

    리뷰컨트롤러 & enum 주석 수정

--- src/main/java/com/damoa/controller/ReviewController.java
+++ src/main/java/com/damoa/controller/ReviewController.java
@@ -1,5 +1,6 @@
 package com.damoa.controller;
 
+import com.damoa.constants.UserType;
 import com.damoa.service.ReviewService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("/review")
+@RequestMapping("/reviews")
 @RequiredArgsConstructor    // DI & final
 public class ReviewController {
 
@@ -18,10 +19,10 @@ public class ReviewController {
     private final ReviewService reviewService;
 
     // Review 메인 요청 페이지
-    // 주소설계: http://localhost:8080/review/home
+    // 주소설계: http://localhost:8080/reviews/home
     // @return review/home 데이터 요청 메서드
     @GetMapping("/home")
-    public String reviewHomePost(Model model) {
+    public String reviewHome(Model model) {
 
         reviewService.getAllReviews(model);
 
@@ -32,15 +33,15 @@ public class ReviewController {
      * 코드 중복 수정
      * 프리랜서, 컴퍼니 리뷰 리스트 페이지 요청
      * URL 쿼리 파라미터로 {type}을 넣어 두 가지 리스트 주소를 처리
-     * 주소설계 : http://localhost:8080/review/list/{type}
+     * 주소설계 : http://localhost:8080/reviews/list/{type}
      * */
     @GetMapping("/list/{type}") // URL의 {type} 부분을 변수로 처리
-    public String reviewListPost(@PathVariable String type, Model model) {
+    public String reviewList(@PathVariable String type, Model model) {
 
         // 타입에 따라 리뷰 목록을 구분하여 추가
-        if ("freelancer".equals(type)) {
+        if (UserType.FREELANCER.name().equalsIgnoreCase(type)) {    // 대소문자 구분 하지 않음
             reviewService.getFreelancerReviews(model);
-        } else if ("company".equals(type)) {
+        } else if (UserType.COMPANY.name().equalsIgnoreCase(type)) {
             reviewService.getCompanyReviews(model);
         } else {
             throw new NullPointerException("없는 페이지 입니다.");  // 파라미터 없을 시 예외 처리
@@ -51,16 +52,16 @@ public class ReviewController {
 
     /*
      * 프리랜서, 컴퍼니 리뷰 상세 페이지 요청
-     * 주소설계 : http://localhost:8080/review/{type}/detail/{id}
+     * 주소설계 : http://localhost:8080/reviews/{type}/detail/{id}
      * @pram type
      * @pram id
      * */
     @GetMapping("/{type}/detail/{id}")
-    public String reviewDetailPost(@PathVariable String type,
-                                   @PathVariable int id, Model model) {
-        if ("freelancer".equals(type)) {
+    public String reviewDetail(@PathVariable String type,
+                               @PathVariable int id, Model model) {
+        if (UserType.FREELANCER.name().equalsIgnoreCase(type)) {
             reviewService.getByFreelancerId(id, model);
-        } else if ("company".equals(type)) {
+        } else if (UserType.COMPANY.name().equalsIgnoreCase(type)) {
             reviewService.getByCompanyId(id, model);
         } else {
             throw new NullPointerException("없는 페이지 입니다.");  // 잘못된 타입 시 예외 처리
